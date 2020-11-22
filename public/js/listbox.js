db6c6eb09b30d6962f9d5e5122d0986453f66df6
Maciej Majchrowski <maciek21197@gmail.com>
2020-11-22 12:27:51 +0100

    Sending form data to the backend

--- public/js/listbox.js
+++ public/js/listbox.js
@@ -1,5 +1,7 @@
 export const dict = ["pin_1", "pin_2", "pin_3", "pin_4"];
 
+const formElem = document.querySelector("form");
+
 export async function generateDBoptions(dbSources) {
   let selectableList = document.getElementById("db");
   setTimeout(() => {
@@ -12,8 +14,26 @@ export async function generateDBoptions(dbSources) {
   }, 2000);
 }
 
-export function submitForm() {
-  fetch("https://api.github.com/users/github")
-    .then((res) => res.json())
-    .then((json) => console.log(json));
-}
+const submitFrom = (event) => {
+  event.preventDefault();
+  new FormData(formElem);
+};
+
+formElem.addEventListener("submit", submitFrom);
+formElem.addEventListener("formdata", (e) => {
+  let data = e.formData;
+  const body = {};
+  for (var [key, value] of data.entries()) {
+    body[key] = value;
+  }
+  console.log(body);
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+    }
+  };
+  xhttp.open("POST", "http://0.0.0.0:5000/xls", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(JSON.stringify(body));
+});
