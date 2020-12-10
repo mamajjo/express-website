20a18f4538752d5810fb2d1b03bb2b9688ec5d25
Maciej Majchrowski <maciek21197@gmail.com>
2020-12-10 09:44:56 +0100

    ADD: result box appears with resposne, colored

--- public/js/listbox.js
+++ public/js/listbox.js
@@ -34,23 +34,24 @@ formElem.addEventListener("formdata", (e) => {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      alert(this.responseText);
-      let message_box = document.getElementsByClassName("message-box-content")[0];
+      let messageBox = document.getElementsByClassName("message-box-content")[0];
+      let hidableCard = document.getElementsByClassName("hidable")[0];
       let textContent = JSON.parse(this.response);
-      message_box.innerHTML = ""
+      messageBox.innerHTML = "";
       let hasErrors = false
       for(const line of textContent['response']) {
         if(line['type'] == "error") {
           hasErrors = true
         }
-        message_box.innerHTML += line['message'] + "\n";
+        messageBox.innerHTML += line["message"] + "<br>";
       }
       if (hasErrors) {
-        changeBoxColor(message_box, "red")
+        changeBoxColor(messageBox, "red");
       }
       else {
-        changeBoxColor(message_box, "green");
+        changeBoxColor(messageBox, "green");
       }
+      uncover(hidableCard);
     }
   };
   xhttp.open("POST", "http://10.0.12.71:5000/xls", true);
@@ -60,4 +61,9 @@ formElem.addEventListener("formdata", (e) => {
 
 function changeBoxColor(element, color) {
   element.style.backgroundColor = color;
+}
+
+function uncover(element) {
+  element.style.visibility = "visible"
+  element.style.opacity = 1;
 }
